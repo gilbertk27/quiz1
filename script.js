31b0aaaca544e2d2aaa071be7e49f25fb83d1ff1
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-09-29 16:52:31 +0700

    update websites

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+    //typing animation
+    var word = new Typed(".typing", {
+        strings: ["Student", "Developer", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var word = new Typed(".typing-2", {
+        strings: ["ITS Surabaya Informatics Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+});
